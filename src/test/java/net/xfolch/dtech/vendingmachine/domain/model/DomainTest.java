f65769bb7317db9de4b52730cf86e7f02f7757b4
Xavi Folch <xfolch@xfolch-mbp.local>
2016-08-07 20:38:20 +0200

    Scale in BigDecimal is already fixed in the domain, so it is useless to put it on testing

--- src/test/java/net/xfolch/dtech/vendingmachine/domain/model/DomainTest.java
+++ src/test/java/net/xfolch/dtech/vendingmachine/domain/model/DomainTest.java
@@ -5,8 +5,6 @@ import net.xfolch.dtech.vendingmachine.domain.model.suppliers.SupplierFactory;
 
 import java.math.BigDecimal;
 
-import static java.math.BigDecimal.ROUND_FLOOR;
-
 /**
  * Created by xfolch on 6/8/16.
  */
@@ -15,19 +13,19 @@ abstract class DomainTest {
     final Product Coke = Product.builder()
             .setId(1)
             .setName("Coke")
-            .setPrice(new BigDecimal(1.50).setScale(2, ROUND_FLOOR))
+            .setPrice(new BigDecimal(1.50))
             .build();
 
     final Product Pepsi = Product.builder()
             .setId(2)
             .setName("Pepsi")
-            .setPrice(new BigDecimal(1.45).setScale(2, ROUND_FLOOR))
+            .setPrice(new BigDecimal(1.45))
             .build();
 
     final Product Water = Product.builder()
             .setId(3)
             .setName("Water")
-            .setPrice(new BigDecimal(0.90).setScale(2, ROUND_FLOOR))
+            .setPrice(new BigDecimal(0.90))
             .build();
 
     final Supplier niceSupplier = SupplierFactory.niceSupplier();
